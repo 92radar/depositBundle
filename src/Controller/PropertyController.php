c9a81fe5854c8c1d0bf9b8e8ad4f96b5f058ef14
92radar <viepro.db@gmail.com>
2026-08-13 17:11:34 +0200

    refactor: simplify PropertyController and PropertyVoter by removing method dependencies and using direct property access

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,6 +4,7 @@ namespace aintreallydown\DepositBundle\Controller;
 
 use aintreallydown\DepositBundle\Form\PropertyFormType;
 use aintreallydown\DepositBundle\Security\Voter\PropertyVoter;
+use App\Entity\Property;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,22 +14,14 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PropertyController extends AbstractController
 {
-    private const STATES = [
-        'address', 'infos', 'rooms', 'energy', 'furnished', 'equipments',
-        'benefits', 'rent', 'charges', 'services', 'extrafields',
-        'availability', 'description', 'title', 'images',
-    ];
-
     public function __construct(
         private EntityManagerInterface $em,
-        private string $propertyClass,
-        private array $methods,
     ) {}
 
     #[Route('/property/{uid}/deposit/', name: 'property.deposit', priority: 1)]
     public function deposit(string $uid, Request $request): Response
     {
-        $property = $this->em->getRepository($this->propertyClass)
+        $property = $this->em->getRepository(Property::class)
             ->findOneBy(['uid' => $uid]);
 
         if ($property === null) {
@@ -37,11 +30,11 @@ class PropertyController extends AbstractController
 
         $this->denyAccessUnlessGranted(PropertyVoter::EDIT, $property);
 
-        $step = $property->{$this->methods['get_state']}();
+        $step = $property->getState();
 
         $slug = 'deposit';
         $nextStep = 'availability';
-        $states = self::STATES;
+        $states = Property::STATES;
 
         if ($step && array_search($step, $states) < array_search($slug, $states)) {
             return $this->redirectToRoute("property.$step", ['uid' => $uid]);
@@ -50,17 +43,17 @@ class PropertyController extends AbstractController
         $form = $this->createForm(PropertyFormType::class);
         $form->handleRequest($request);
 
-        $extrafields = $property->{$this->methods['get_extrafields']}() ?? [];
+        $extrafields = $property->getExtrafields() ?? [];
 
         if ($form->isSubmitted() && $form->isValid()) {
 
             $extrafields['deposit'] = $form->get('deposit')->getData();
-            $property->{$this->methods['set_extrafields']}($extrafields);
+            $property->setExtrafields($extrafields);
 
             $isSave = $form->get('save')->isClicked();
 
             if ($step === $slug && !$isSave) {
-                $property->{$this->methods['set_state']}($nextStep);
+                $property->setState($nextStep);
             }
 
             $this->em->flush();
@@ -70,11 +63,11 @@ class PropertyController extends AbstractController
                 : $this->redirectToRoute("property.$nextStep", ['uid' => $uid]);
         }
 
-        $default = ($property->{$this->methods['get_rent']}()) - ($property->{$this->methods['get_charges']}());
-        
-        $max = ($property->{$this->methods['is_furnished']}())
-        ? $default * 2 
-        : $default;
+        $default = $property->getRent() - $property->getCharges();
+
+        $max = $property->isFurnished()
+            ? $default * 2
+            : $default;
 
         $deposit = $default === null
             ? $max
--- src/Security/Voter/PropertyVoter.php
+++ src/Security/Voter/PropertyVoter.php
@@ -2,6 +2,7 @@
 
 namespace aintreallydown\DepositBundle\Security\Voter;
 
+use App\Entity\Property;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -11,15 +12,10 @@ class PropertyVoter extends Voter
     public const EDIT = 'PROPERTY_EDIT';
     public const VIEW = 'PROPERTY_VIEW';
 
-    public function __construct(
-        private string $propertyClass,
-        private array $methods,
-    ) {}
-
     protected function supports(string $attribute, mixed $subject): bool
     {
         return in_array($attribute, [self::EDIT, self::VIEW])
-            && $subject instanceof $this->propertyClass;
+            && $subject instanceof Property;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
@@ -32,10 +28,10 @@ class PropertyVoter extends Voter
 
         switch ($attribute) {
             case self::EDIT:
-                $owner = $subject->{$this->methods['get_owner']}();
+                $owner = $subject->getOwner();
 
                 return $owner !== null
-                    && $owner->{$this->methods['get_uid']}() === $user->{$this->methods['get_uid']}();
+                    && $owner->getUid() === $user->getUid();
 
             case self::VIEW:
                 break;
